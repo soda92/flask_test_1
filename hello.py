0f3c2b9da1bc3b4ae1b91010c94cf3af3b8728a3
SodaCris <18463922396@163.com>
2021-11-22 08:54:30 +0800

    add template file

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/map')
+def map(name=None):
+    return render_template('map.html')
